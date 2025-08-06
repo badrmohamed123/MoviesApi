cb53621eb6a9738cfb49ca22731872d5eda60435
badrmohamed123 <167132960+badrmohamed123@users.noreply.github.com>
2025-08-06 11:00:24 +0300

    Add project files.

---
+++ MoviesApi/Program.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddCors();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo 
+    { 
+        Version="v1",
+        Title = "TestApi",
+        Description = "My Api",
+        Contact = new OpenApiContact
+        {
+            Name = "Badr",
+            Email = "Badr.com"
+
+        },
+        License = new OpenApiLicense { 
+            Name = "My License", 
+            Url = new Uri("https://example.com/license") 
+        }
+    });
+    // هنا بنستخدم ال Authorization علي كل ال اند بوينت من فوق بنضيف التوكن مره واحده بس 
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+    });
+    //نستخدم هنا ال Authorization علامه القفل علي كل اند بوينت لوحدها  
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Name =  "Bearer",
+                In = ParameterLocation.Header
+            },
+                new List<string>()
+        }
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseCors(c => c.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
